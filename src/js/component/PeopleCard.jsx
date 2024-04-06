50a6814d12d6a71810e7ae9b2ef8ba477a489abe
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-04-06 04:24:05 +0000

    changed Card variables info

--- src/js/component/PeopleCard.jsx
+++ src/js/component/PeopleCard.jsx
@@ -3,29 +3,30 @@ import { useParams } from "react-router-dom";
 import { Context } from "../store/appContext";
 
 export const PeopleCard = () => {
-    const { uid } = useParams(); // Extraer el UID de la URL
-    const { store } = useContext(Context);
+  const { uid } = useParams(); // Extraer el UID de la URL
+  const { store } = useContext(Context);
 
-    // Encontrar la persona correspondiente al UID proporcionado
-    const person = store.peopleDetails.find(person => person.result.uid === uid);
+  // Encontrar la persona correspondiente al UID proporcionado
+  const person = store.peopleDetails.find(person => person.result.uid === uid);
 
-    // Verificar si la persona fue encontrada
-    if (!person) {
-        return <div>No person found for UID {uid}</div>;
-    }
+  // Verificar si la persona fue encontrada
+  if (!person) {
+    return <div>No person found for UID {uid}</div>;
+  }
 
-    return (
-        <div className="card mb-3">
-            <img src="..." className="card-img-top" alt="..." />
-            <div className="card-body">
-                <h5 className="card-title">{person.result.properties.name}</h5>
-                <p className="card-text">Gender: {person.result.properties.gender}</p>
-                <p className="card-text">Height: {person.result.properties.height}</p>
-                <p className="card-text">Birth Year: {person.result.properties.birth_year}</p>
-                {/* Otros detalles de la persona */}
-            </div>
-        </div>
-    );
+  return (
+    <div className="card mb-3">
+      <img src="..." className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{person.result.properties.name}</h5>
+        <p className="card-text">Mass: {person.result.properties.mass}</p>
+        <p className="card-text">Hair Color: {person.result.properties.hair_color}</p>
+        <p className="card-text">Skin Color: {person.result.properties.skin_color}</p>
+        <p className="card-text">Eye Color: {person.result.properties.eye_color}</p>
+        {/* Otros detalles de la persona */}
+      </div>
+    </div>
+  );
 };
 
 
